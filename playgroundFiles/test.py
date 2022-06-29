999cf9e9e346d479c4094f8955e9103484aecbff
BKrishnan123 <krishnan.bharatp@gmail.com>
2022-06-28 20:48:56 -0400

    tings4

---
+++ playgroundFiles/test.py
@@ -0,0 +1,102 @@
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import time
+import mediapipe as mp
+
+mp_holistic = mp.solutions.holistic # Holistic model
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.FACEMESH_TESSELATION) # Draw pose connections
+
+def draw_styled_landmarks(image, results):
+
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                             ) 
+
+# cap = cv2.VideoCapture('testFootage.mp4')
+# # Loop through each frame
+# with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+#     for frame_idx in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
+#         # Read feed
+#         ret, frame = cap.read()
+
+#         # Make detections
+#         image, results = mediapipe_detection(frame, holistic)
+#         print(results)
+        
+#         # Draw landmarks
+#         draw_styled_landmarks(image, results)
+
+#         # Show to screen
+#         cv2.imshow('OpenCV Feed', image)
+
+#         # Break gracefully
+#         if cv2.waitKey(10) & 0xFF == ord('q'):
+#             break
+#     cap.release()
+#     cv2.destroyAllWindows()
+
+cap = cv2.VideoCapture('testFootage.mp4')
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # Flip the image horizontally for a later selfie-view display, and convert
+    # the BGR image to RGB.
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    results = holistic.process(image)
+
+    # Draw the pose annotation on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    mp_drawing.draw_landmarks(
+        image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    cv2.imshow('MediaPipe Pose', image)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+      break
+cap.release()
+
+# cap = cv2.VideoCapture(0)
+# # Set mediapipe model 
+# with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+#     while cap.isOpened():
+
+#         # Read feed
+#         ret, frame = cap.read()
+
+#         # Make detections
+#         image, results = mediapipe_detection(frame, holistic)
+#         print(results)
+        
+#         # Draw landmarks
+#         draw_styled_landmarks(image, results)
+
+#         # Show to screen
+#         cv2.imshow('OpenCV Feed', image)
+
+#         # Break gracefully
+#         if cv2.waitKey(10) & 0xFF == ord('q'):
+#             break
+#     cap.release()
+#     cv2.destroyAllWindows()
