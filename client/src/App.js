0f24daafce908073d5f39a364fb295197c79aa24
JaiNarayanan <47904405+JaiNarayanan@users.noreply.github.com>
2022-07-31 12:13:25 -0400

    update client structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef} from 'react'
+import './style.css'
+import { Container, Row, Col, Button, Icon, Navbar, NavItem } from 'react-materialize';
+import gloves from './assets/images/redGloves.png'
+// import idealGloves from './assets/images/idealGloves.png'
+import Texture from './components/Texture'
+import 'materialize-css';
 
-function App() {
+const App = () => {
+  const myRef = useRef(null)
+
+  const executeScroll = () => myRef.current.scrollIntoView({ behavior: 'smooth' })   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Container>
+        <Row/>
+        <Navbar className="customNav red z-depth-0 lighten-1"
+          alignLinks="right"
+          brand={<a className="brand-logo" href="#"></a>}
+          id="mobile-nav"
+          menuIcon={<Icon>menu</Icon>}
+          options={{
+            draggable: true,
+            edge: 'left',
+            inDuration: 250,
+            onCloseEnd: null,
+            onCloseStart: null,
+            onOpenEnd: null,
+            onOpenStart: null,
+            outDuration: 200,
+            preventScrolling: true
+          }}
         >
-          Learn React
+          <NavItem className = "customNavItem" href="">
+            about
+          </NavItem>
+          <NavItem className = "customNavItem" href="">
+            demo 
+          </NavItem>
+          <NavItem className = "customNavItem" href="">
+            login
+          </NavItem>
+        </Navbar>
+
+        <Row className="above" />
+        <Row className="center-align">
+        <h1 className="title">COMBAT.AI</h1>
+
+          <Button onClick={executeScroll} className="subtitleButton z-depth-0">Pick up the gloves</Button>
+          {/* <h5 className = "subtitle">Pick up the gloves</h5> */}
+
+        </Row>
+        <Row>
+        <a onClick={executeScroll}>
+            <img className="floating" width="128" src={gloves} alt="gloves" />
         </a>
-      </header>
+        </Row>
+      </Container>
+
+      <Texture/>
+      <div ref = {myRef}></div>
+          
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/components/Texture.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Texture = () => {
+  return (
+    <div>
+      <div className = "above"></div>
+      <h4 className = "texture">
+    "I love to hit people. I love to. Most celebrities are afraid someone's going to attack them. I want someone to attack me. No weapons. Just me and him."
+    "Real freedom is having nothing. I was freer when I didn’t have a cent."
+    “I’m addicted to perfection. Problem with my life is I was always also addicted to chaos. Perfect chaos.”
+    “I just have this thing inside me that wants to eat and conquer. Maybe it's egotistical, but I have it in me. I don't want to be a tycoon. I just want to conquer people and their souls."
+    "I love to hit people. I love to. Most celebrities are afraid someone's going to attack them. I want someone to attack me. No weapons. Just me and him."
+    "Real freedom is having nothing. I was freer when I didn’t have a cent."
+    “I’m addicted to perfection. Problem with my life is I was always also addicted to chaos. Perfect chaos.”
+    “I just have this thing inside me that wants to eat and conquer. Maybe it's egotistical, but I have it in me. I don't want to be a tycoon. I just want to conquer people and their souls."
+    "I love to hit people. I love to. Most celebrities are afraid someone's going to attack them. I want someone to attack me. No weapons. Just me and him."
+    "Real freedom is having nothing. I was freer when I didn’t have a cent."
+    “I’m addicted to perfection. Problem with my life is I was always also addicted to chaos. Perfect chaos.”
+    “I just have this thing inside me that wants to eat and conquer. Maybe it's egotistical, but I have it in me. I don't want to be a tycoon. I just want to conquer people and their souls."
+    "I love to hit people. I love to. Most celebrities are afraid someone's going to attack them. I want someone to attack me. No weapons. Just me and him."
+    "Real freedom is having nothing. I was freer when I didn’t have a cent."
+    “I’m addicted to perfection. Problem with my life is I was always also addicted to chaos. Perfect chaos.”
+    “I just have this thing inside me that wants to eat and conquer. Maybe it's egotistical, but I have it in me. I don't want to be a tycoon. I just want to conquer people and their souls."
+    "I love to hit people. I love to. Most celebrities are afraid someone's going to attack them. I want someone to attack me. No weapons. Just me and him."
+    {/* "Real freedom is having nothing. I was freer when I didn’t have a cent." */}
+    {/* “I’m addicted to perfection. Problem with my life is I was always also addicted to chaos. Perfect chaos.” */}
+    {/* “I just have this thing inside me that wants to eat and conquer. Maybe it's egotistical, but I have it in me. I don't want to be a tycoon. I just want to conquer people and their souls." */}
+
+    </h4></div>
+  )
+}
+
+export default Texture
